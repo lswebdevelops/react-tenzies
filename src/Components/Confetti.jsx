720e2070a59ba65478bd5a9880b5a4ed3a6fa854
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-15 22:59:43 -0300

    adding files for first commit

---
+++ src/Components/Confetti.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Confetti(props) {
+    const styles = {
+        backgroundColor: props.held ? "#59E391" : "white"
+    }
+    return (
+       <h2>🎇🎇🎇🎇🎇🎇🎇🎇🎇🎇🎇🎇🎇🎇</h2>
+    )
+}
